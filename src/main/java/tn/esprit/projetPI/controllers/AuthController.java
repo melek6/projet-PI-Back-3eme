e3407965d0399d882f977f3751f028f560aa417a
Terzimanel <135385644+Terzimanel@users.noreply.github.com>
2024-07-04 22:47:31 +0200

    update inscription

--- src/main/java/tn/esprit/projetPI/controllers/AuthController.java
+++ src/main/java/tn/esprit/projetPI/controllers/AuthController.java
@@ -110,6 +110,62 @@ userDetails.isBlocked(),
 
 				roles));
 	}
+	@PostMapping("/signuUUUUp")
+	public ResponseEntity<?> registerUserRRRR(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getUsername(),
+				signUpRequest.getEmail(),
+				encoder.encode(signUpRequest.getPassword()));
+
+		Set<String> strRoles = signUpRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+					case "admin":
+						Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+								.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+						roles.add(adminRole);
+
+						break;
+					case "mod":
+						Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+								.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+						roles.add(modRole);
+
+						break;
+					default:
+						Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+								.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+						roles.add(userRole);
+				}
+			});
+		}
+
+		user.setRoles(roles);
+		userRepository.save(user);
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+
+
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
 		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
@@ -167,11 +223,13 @@ userDetails.isBlocked(),
 		userRepository.save(user);
 
 		// Send verification email
-		String verificationUrl = "http://yourdomain.com/verify?token=" + verificationToken;
+		String verificationUrl = "http://localhost:4200/#/verif?token=" + verificationToken;
 		emailService.sendSimpleEmail(user.getEmail(), "Email Verification", "Please verify your email by clicking on the following link: " + verificationUrl);
 
 		return ResponseEntity.ok(new MessageResponse("User registered successfully! Please check your email to verify your account."));
 	}
+
+
 	@GetMapping("/verify")
 	public ResponseEntity<?> verifyUser(@RequestParam("token") String token) {
 		User user = userRepository.findByVerificationToken(token)
